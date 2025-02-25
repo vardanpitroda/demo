51c8620dc187a6e14864845026af42760f37c0bb
vardanpitroda <vardanpitroda007@gmail.com>
2025-02-25 15:25:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Form from "./components/form";
+import Form2 from "./components/form2";
+import Table from "./components/table";
 
 function App() {
-
   const user = {
-    name: 'John Doe',
-    age: 30
-  }
+    name: "John Doe",
+    age: 30,
+  };
 
-  function getData(data){
-    console.log(data)
+  function getData(data) {
+    console.log(data);
   }
-
   return (
     <div>
-        <BrowserRouter>
-        <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home user={user}/>}/>
-            <Route path='/about' element={<About getData={getData}/>}/>
-            <Route path='/contact' element={<Contact/>}/>
-            <Route path='/form' element={<Form setData={setData}/>}/>
-              <Route path='/table' element={<Table data={data}/>}/>
-              <Route path='/form2' element={<Form2/>}/>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home user={user} />} />
+          <Route path="/about" element={<About getData={getData} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/form" element={<Form setData={setData} />} />
+          <Route path="/table" element={<Table data={data} />} />
+          <Route path="/form2" element={<Form2 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
